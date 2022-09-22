ed94e4ea85ea6e8f1b729abd77cd976ac5d1bad0
Alexey Mostepanenko <fattouch143@yandex.ru>
2022-09-22 19:57:20 +0400

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+WriteLine("Введите 2 числа для cравнения: ");
+
+int a = Convert.ToInt32(ReadLine());
+int b = Convert.ToInt32(ReadLine());
+
+if(a>b)
+{
+    WriteLine(a);
+}
+if(a<b)
+{
+    WriteLine(b);
+}
+else
+{
+    WriteLine("Числа равны");
+}
